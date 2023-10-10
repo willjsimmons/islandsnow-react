61b996d14f19be39e529752d9f53e770986b3964
willjsimmons <willjsimmons@gmail.com>
2023-10-09 17:54:17 -1000

    Task 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import { Container } from 'react-bootstrap';
 
 const IslandSnow = () => (
-  <h1>Island Snow!</h1>
+    <Container className="text-center">
+        <h1>Island Snow!</h1>
+    </Container>
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
