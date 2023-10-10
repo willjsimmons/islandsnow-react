326074deb1896f8ab8b66910a5fe1bc094b580b0
willjsimmons <willjsimmons@gmail.com>
2023-10-09 17:50:42 -1000

    Task 2

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+const IslandSnow = () => (
+  <h1>Island Snow!</h1>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<IslandSnow />);
