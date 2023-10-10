e56ad4fbdd7b81c852f13efd44d133d4d0d70e89
willjsimmons <willjsimmons@gmail.com>
2023-10-09 17:56:37 -1000

    Task 4 start template

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import { Container } from 'react-bootstrap';
 
+const TopMenu = () => (
+    <h1>TopMenu</h1>
+);
+
+const IslandSnowLogo = () => (
+    <h1>IslandSnowLogo</h1>
+);
+
+const MiddleMenu = () => (
+    <h1>MiddleMenu</h1>
+);
+
+const FullWidthImage = () => (
+    <h1>FullWidthImage</h1>
+);
+
+const FooterMenu = () => (
+    <h1>FooterMenu</h1>
+);
+
 const IslandSnow = () => (
-    <Container className="text-center">
-        <h1>Island Snow!</h1>
+    <Container>
+        <TopMenu/>
+        <IslandSnowLogo/>
+        <MiddleMenu/>
+        <FullWidthImage/>
+        <FooterMenu/>
     </Container>
 );
 
